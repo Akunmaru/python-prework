10dca777945d9b3457469c510924dc5cea3da4b5
Akunmaru <akunmaru@Akuns-MacBook-Air.local>
2023-01-31 09:09:15 -0700

    first commit

---
+++ my_prework.py
@@ -0,0 +1,7 @@
+#Question 1
+#write a f(x) to print "hello_USERNAME!" USERNAME is the input of the f(X)
+
+def hello(user_name):
+    print(f"Hello {user_name}!")
+    return
+print(hello(('Akunmaru')))
